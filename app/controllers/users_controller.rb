26034e8db8749bbd8ebf075b0ee7e12546e31fdd
Steve Jones <steve.jones763@gmail.com>
2021-11-03 14:04:24 +0000

    Installed Rubocop and fixed offences.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(params.require(:user).permit(:username,        
       :password))
-      session[:user_id] = @user.id
-      redirect_to '/newsfeed'
+    session[:user_id] = @user.id
+    redirect_to '/newsfeed'
   end
 end
