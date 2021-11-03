0827149aa185cbb26e4451d15b930fd18a32699a
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-03 16:12:40 +0000

    HP_feature_05: Added feature test for navigation bar on new post page, updated CSS for new post page

--- spec/features/logged_in_users_can_see_navigation_bar_spec.rb
+++ spec/features/logged_in_users_can_see_navigation_bar_spec.rb
@@ -1,10 +1,20 @@
 RSpec.feature "Navigation Bar", type: :feature do
-  scenario "logged in users can see the navigation bar" do
+
+  scenario "logged in users can see the navigation bar on the newsfeed page" do
     sign_up_test_user
     Capybara.reset_session!
     log_in_user
-    visit('/newsfeed')
     expect(page).to have_content 'BOOKAROO'
     expect(page).to have_button 'Log out'
   end
+
+  scenario "logged in users can see the nav bar on the new post page" do
+    sign_up_test_user
+    Capybara.reset_session!
+    log_in_user
+    click_link 'New post'
+    expect(page).to have_content 'BOOKAROO'
+    expect(page).to have_button 'Log out'
+  end
+
 end
