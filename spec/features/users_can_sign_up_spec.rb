397c0fd601e508cbdb2c8050889bd723b0cbe273
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-04 16:47:30 +0000

    HP_feature_10: Added feature test for profile picture field, added profile_picture to users_controller and view, added column to users database

--- spec/features/users_can_sign_up_spec.rb
+++ spec/features/users_can_sign_up_spec.rb
@@ -5,4 +5,12 @@ RSpec.feature "Sign up", type: :feature do
     sign_up_test_user
     expect(page).to have_current_path("/newsfeed")
   end
+
+  scenario "New users can fill in the sign up form" do
+    visit "/"
+    click_button "Sign up"
+    expect(page).to have_current_path("/sign-up")
+    expect(page).to have_content("Profile picture")
+  end
+
 end
